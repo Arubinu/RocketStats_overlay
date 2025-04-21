0cb8767d5a9ed488661ed155e76fe902c5c675c4
Hazar <hazarcar0353@gmail.com>
2025-04-21 17:21:55 +0300

    Rank relocation provided
    You can choose ready css properties from config file.
    Currently there are two ready (boost, side)
    You can create your own css property.

--- themes/Circle/script.js
+++ themes/Circle/script.js
@@ -25,6 +25,22 @@ files[ 'Background' ] = {
 window[ 'themeInit' ] = () => {
 	if ( getOption( 'background_video' ) )
 		document.querySelector( '#container' ).classList.add( 'video' );
+
+	if ( getOption( 'rank_location' ) && getOption( 'display_rank' ) ) {
+		let rank_location = getOption( 'rank_location' )
+		const rankImage = document.querySelector( '[file-target="RocketStats_Rank"]' );
+		if ( rankImage ) {
+			if ( rank_location == "boost" ) {
+				rankImage.classList.add( 'boost' );
+			} else if ( rank_location == "side" ) {
+				rankImage.classList.add( 'side' );
+			} else {
+				console.warn('Invalid rank location specified. Choose it in this list "boost, side" or leave empty.');
+			}
+		} else {
+			console.warn('RocketStats_Rank element not found.');
+		}
+	};
 };
 
 // Function called by the overlay after the elements have been created
